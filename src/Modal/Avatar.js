b85534e8d5279b22972f647294a6fda03c5eeb3b
sorano <rokudo.ananas.mukuro@gmail.com>
2021-07-16 17:23:02 +0300

    дорабока

--- src/Modal/Avatar.js
+++ src/Modal/Avatar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import PropTypes from 'prop-types'
 
 
 function Avatar({SetAvatarP1,SetAvatarP2,close,player1,player2}){
@@ -9,7 +9,7 @@ function Avatar({SetAvatarP1,SetAvatarP2,close,player1,player2}){
          <div className='avatar__body'>
             <h2>Choose Avatar</h2>
             <div className='avatar__title'>
-            <h2>Avatar for: {player2}</h2>
+            <h2>Avatar for: {player1}</h2>
             </div>
             <div className='avatar__container'>
                <img className='avatar__img' src='./avatar1.png' alt='avatar' onClick={event => SetAvatarP1(event.target)}></img>
@@ -30,5 +30,7 @@ function Avatar({SetAvatarP1,SetAvatarP2,close,player1,player2}){
    )
 
 }
-
+Avatar.propTypes ={
+   onCreate: PropTypes.func.isRequired
+}
 export default Avatar
